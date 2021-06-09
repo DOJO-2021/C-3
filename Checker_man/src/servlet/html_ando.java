941da4adaa6dcc37fed776132829c758e8f8c7cc
dojo2021-k-ando <ando@seplus2016.onmicrosoft.com>
2021-06-09 16:48:01 +0900

    変更

--- Checker_man/src/servlet/html_ando.java
+++ Checker_man/src/servlet/html_ando.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet("/html_suzuki")
+@WebServlet("/html_ando")
 public class html_ando extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
