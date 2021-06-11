eee5c76adec4104b6408f745870ac178dad83ee2
dojo2021-M-Suzuki <suzuki-ma@seplus2016.onmicrosoft.com>
2021-06-11 10:47:51 +0900

    更新

--- Checker_man/src/model/s_question.java
+++ Checker_man/src/model/s_question.java
@@ -8,37 +8,13 @@ public class s_question implements Serializable {
 	// 質問項目を保持するフィールド
 	private String Question;
 
-	public Result() {
-		this(null, null, null);
+	// 質問項目を表示するメソッド
+	public void showQuestion() {
+		System.out.println(this.Question);
 	}
 
-	public Result(String title, String message, String backTo) {
-		this.title = title;
-		this.message = message;
-		this.backTo = backTo;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getMessage() {
-		return message;
-	}
-
-	public void setMessage(String message) {
-		this.message = message;
-	}
-
-	public String getBackTo() {
-		return backTo;
-	}
-
-	public void setBackTo(String backTo) {
-		this.backTo = backTo;
+	// コンストラクタ
+	public s_question(String Question) {
+		this.Question = Question;
 	}
 }
