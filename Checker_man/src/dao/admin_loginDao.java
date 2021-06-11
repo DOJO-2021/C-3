4c3bac84a20acfda9519720fe6c93dcbcd74bf21
dojo2021-k-ando <ando@seplus2016.onmicrosoft.com>
2021-06-11 11:08:59 +0900

    変更

---
+++ Checker_man/src/dao/admin_loginDao.java
@@ -0,0 +1,66 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class admin_loginDao {
+
+	// ログインできるならtrueを返す
+	public boolean isLoginOK(String id, String pw) {
+
+		Connection conn = null;
+		boolean loginResult = false;
+
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("org.h2.Driver");
+
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/C-3", "sa", "");
+
+			// SELECT文を準備する
+			String sql = "select count(*) from IDPW where ID = ? and PW = ?";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+			pStmt.setString(1, id);
+			pStmt.setString(2, pw);
+
+			// SELECT文を実行し、結果表を取得する
+			ResultSet rs = pStmt.executeQuery();
+
+			// 管理者IDとパスワードが一致する管理者がいたかどうかをチェックする
+			rs.next();
+			if (rs.getInt("count(*)") == 1) {
+				loginResult = true;
+			}
+		}
+
+		catch (SQLException e) {
+			e.printStackTrace(); //例外情報を表示
+			loginResult = false;
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			loginResult = false;
+		}
+
+		finally { //例外があってもなくても、最後にやる共通の処理
+
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				}
+				catch (SQLException e) {
+					e.printStackTrace();
+					loginResult = false;
+				}
+			}
+		}
+
+		// 結果を返す
+		return loginResult;
+	}
+}
