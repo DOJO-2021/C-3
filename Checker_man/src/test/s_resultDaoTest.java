81c081fd5138864bbd1f299937215cb44035c098
dojo2021-n-hoshi <hoshi@seplus2016.onmicrosoft.com>
2021-06-14 13:27:22 +0900

    更新

---
+++ Checker_man/src/test/s_resultDaoTest.java
@@ -0,0 +1,63 @@
+package test;
+import java.util.List;
+
+import dao.s_answerDao;
+import model.admin_toppage;
+import model.s_answer;
+import model.s_result;
+
+public class s_resultDaoTest {
+
+	public static void main(String[] args) {
+		s_result dao = new s_resultDao();
+
+		// insert()のテスト
+				System.out.println("---------- insert()のテスト ----------");
+
+				s_result insRec = new s_result(0, null, "maru", "受講者コメント", "管理者コメント", 6);
+
+				if (dao.insert(insRec)) {
+					System.out.println("登録成功！");
+
+					List<s_result> resultList = dao.select(insRec);
+
+					for (s_result resultData : resultList) {
+						System.out.println("result_id：" + resultData.getResult_id());
+						System.out.println("date：" + resultData.getDate());
+						System.out.println("icon：" + resultData.getIcon());
+						System.out.println("user_comment：" + resultData.getUser_comment());
+						System.out.println("admin_comment：" + resultData.getAdmin_comment());
+						System.out.println("user_id：" + resultData.getUser_id());
+						System.out.println();
+					}
+				}
+
+				else {
+					System.out.println("登録失敗！");
+				}*/
+
+				// selectのテスト
+						System.out.println("---------- selectのテスト ----------");
+
+						List<s_result> resultList2 = dao.select(new s_result(0, "2021", "", "", "", 0));
+
+						for (s_result resultData : resultList2) {
+							System.out.println("result_id：" + resultData.getResult_id());
+							System.out.println("date：" + resultData.getDate());
+							System.out.println("icon：" + resultData.getIcon());
+							System.out.println("user_comment：" + resultData.getUser_comment());
+							System.out.println("admin_comment：" + resultData.getAdmin_comment());
+							System.out.println("user_id：" + resultData.getUser_id());
+							System.out.println();
+						}
+
+						// select_iconのテスト
+						System.out.println("---------- select_iconのテスト ----------");
+
+						admin_toppage resultData = dao.select_icon(new admin_toppage("sankaku", 1));;
+
+							System.out.println("icon：" + resultData.getIcon());
+							System.out.println("icon_number：" +resultData.getIcon_number());
+							System.out.println();
+	}
+}
