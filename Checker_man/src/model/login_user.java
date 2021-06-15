f5a5e796a40dc867c27da5a33f9c3f51bdc114ae
dojo2021-n-hoshi <hoshi@seplus2016.onmicrosoft.com>
2021-06-15 16:57:18 +0900

    更新

--- Checker_man/src/model/login_user.java
+++ Checker_man/src/model/login_user.java
@@ -2,17 +2,17 @@ package model;
 
 import java.io.Serializable;
 
-public class LoginUser implements Serializable {
+public class login_user implements Serializable {
 
 	private String user_id;	// ログイン時のID
 
 	//引数がないコントラスタ（デフォルトコントラスタ）
-	public LoginUser() {
+	public login_user() {
 		this(null);
 	}
 
 	//引数があるコントラスタ
-	public LoginUser(String user_id) {
+	public login_user(String user_id) {
 		this.user_id = user_id;
 	}
 
