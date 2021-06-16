efb716dd987be430f40a0d8cfed71cff3bdaa215
dojo2021-M-Suzuki <suzuki-ma@seplus2016.onmicrosoft.com>
2021-06-16 15:46:32 +0900

    変更

--- Checker_man/src/servlet/User_SindanServlet.java
+++ Checker_man/src/servlet/User_SindanServlet.java
@@ -10,9 +10,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import dao.s_resultDao;
 import model.Result;
-import model.s_result;
+import model.s_answer;
 
 @WebServlet("/User_SindanServlet")
 public class User_SindanServlet extends HttpServlet {
@@ -38,7 +37,7 @@ public class User_SindanServlet extends HttpServlet {
 
 		// もしもログインしていなかったらログインサーブレットにリダイレクトする
 		HttpSession session = request.getSession();
-		if (session.getAttribute("id") == null) {
+		if (session.getAttribute("user_id") == null) {
 			response.sendRedirect("/Checker_man/User_LoginServlet");
 			return;
 		}
@@ -46,12 +45,14 @@ public class User_SindanServlet extends HttpServlet {
 		// リクエストパラメータを取得する
 		request.setCharacterEncoding("UTF-8");
 		String question_id = request.getParameter("QUESTION_ID");
+		String anwer = request.getParameter("ANSWER");
+		String user_id = request.getParameter("USER_ID");
 		String question = request.getParameter("QUESTION");
 
 		// 登録処理を行う
-		s_resultDao bDao = new s_resultDao();
+		s_answer bDao = new s_answer();
 		if (bDao.insert(
-				new s_result(question_id, question))) { // 登録成功
+				new s_answer(0, 0, 0, question))) { // 登録成功
 			request.setAttribute("result",
 					new Result("登録成功", "レコードを登録しました。", "/Checker_man/User_ResultServlet"));
 		} else { // 登録失敗
@@ -59,7 +60,7 @@ public class User_SindanServlet extends HttpServlet {
 					new Result("登録失敗", "レコードを登録できませんでした。", "/Checker_man/User_SindanServlet"));
 		}
 
-		// 結果ページにフォワードする
+		// 診断結果ページにフォワードする
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/User_Result.jsp");
 		dispatcher.forward(request, response);
 	}
