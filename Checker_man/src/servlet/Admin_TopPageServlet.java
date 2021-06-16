59f23c16d8ab1b21e79727e4fbcc3cc3ac7f6bb4
dojo2021-n-hoshi <hoshi@seplus2016.onmicrosoft.com>
2021-06-16 13:57:20 +0900

    削除

--- Checker_man/src/servlet/Admin_TopPageServlet.java
+++ Checker_man/src/servlet/Admin_TopPageServlet.java
@@ -1,7 +1,6 @@
 package servlet;
 
 import java.io.IOException;
-import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -10,12 +9,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import dao.s_resultDao;
-import model.user_toppage;
 
-
-@WebServlet("/User_TopPageServlet")
-public class User_TopPageServlet extends HttpServlet {
+@WebServlet("/Admin_TopPageServlet")
+public class Admin_TopPageServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -23,37 +19,37 @@ public class User_TopPageServlet extends HttpServlet {
 
 	//	 もしもログインしていなかったらログインサーブレットにリダイレクトする
 //				HttpSession session = request.getSession();
-//				if (session.getAttribute("user_id") == null) {
-//					response.sendRedirect("/Checker_man/User_LoginServlet");
+//				if (session.getAttribute("admin_id") == null) {
+//					response.sendRedirect("/Checker_man/Admin_LoginServlet");
 //					return;
 //				}
 
 
 
-		// カレンダーの検索処理を行う（ユーザID = 1の場合）
-				s_resultDao calenderDao = new s_resultDao();
-				List<user_toppage> calenderlist = calenderDao.select_calender(new user_toppage("", "",1));
-
-		// カレンダーの検索処理を行う
-//				HttpSession session = request.getSession();
+//		// カレンダーの検索処理を行う（ユーザID = 1の場合）
 //				s_resultDao calenderDao = new s_resultDao();
-//				int id = (int)session.getAttribute("user_id");
-//				List<user_toppage> calenderlist = calenderDao.select_calender(new user_toppage("", "",id));
+//				List<user_toppage> calenderlist = calenderDao.select_calender(new user_toppage("", "",1));
+//
+//		// カレンダーの検索処理を行う
+////				HttpSession session = request.getSession();
+////				s_resultDao calenderDao = new s_resultDao();
+////				int id = (int)session.getAttribute("user_id");
+////				List<user_toppage> calenderlist = calenderDao.select_calender(new user_toppage("", "",id));
+//
+//		// カレンダーの処理をリクエストスコープに格納する
+//		request.setAttribute("calenderlist", calenderlist);
 
-		// カレンダーの処理をリクエストスコープに格納する
-		request.setAttribute("calenderlist", calenderlist);
-
-		// ユーザートップページにフォワードする
-		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/user_toppage.jsp");
+		// 管理者トップページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/admin_toppage.jsp");
 		dispatcher.forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-//		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+		//	 もしもログインしていなかったらログインサーブレットにリダイレクトする
 //		HttpSession session = request.getSession();
-//		if (session.getAttribute("user_id") == null) {
-//			response.sendRedirect("/Checker_man/User_LoginServlet");
+//		if (session.getAttribute("admin_id") == null) {
+//			response.sendRedirect("/Checker_man/Admin_LoginServlet");
 //			return;
 //		}
 
