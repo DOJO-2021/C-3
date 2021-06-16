659c842b5f7f2cdbd36c63ee9a94ecab781ec512
dojo2021-n-hoshi <hoshi@seplus2016.onmicrosoft.com>
2021-06-16 10:48:38 +0900

    追加

---
+++ Checker_man/src/model/LoginUser.java
@@ -0,0 +1,29 @@
+package model;
+
+import java.io.Serializable;
+
+public class LoginUser implements Serializable {
+
+	private String user_id;	// ログイン時のID
+
+	//引数がないコントラスタ（デフォルトコントラスタ）
+	public LoginUser() {
+		this(null);
+	}
+
+	//引数があるコントラスタ
+	public LoginUser(String user_id) {
+		this.user_id = user_id;
+	}
+
+	//ゲッターセッターのプログラム
+	public String getuser_id() {
+		return user_id;
+	}
+
+	public void setuser_id(String user_id) {
+		this.user_id = user_id;
+	}
+
+
+}
