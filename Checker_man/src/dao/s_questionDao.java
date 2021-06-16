52debe9bbb7ada07769c14786081c2dc2ae39563
dojo2021-M-Suzuki <suzuki-ma@seplus2016.onmicrosoft.com>
2021-06-16 15:21:18 +0900

    変更

--- Checker_man/src/dao/s_questionDao.java
+++ Checker_man/src/dao/s_questionDao.java
@@ -17,38 +17,23 @@ public class s_questionDao {
 		Connection conn = null;
 		List<s_question> questionList = new ArrayList<s_question>();
 
-		// JDBCドライバを読み込む
 		try {
+			// JDBCドライバを読み込む
 			Class.forName("org.h2.Driver");
-		} catch (ClassNotFoundException e1) {
 
-			e1.printStackTrace();
-		}
-
-		// データベースに接続する
-		try {
+			// データベースに接続する
 			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/C-3/database", "sa",
 					"sa");
-		} catch (SQLException e1) {
 
-			e1.printStackTrace();
-		}
-
-		// SELECT文を準備する
-		String sql = "select * from s_question";
-		PreparedStatement pStmt = null;
-		try {
+			// SELECT文を準備する
+			String sql = "select * from s_question";
+			PreparedStatement pStmt = null;
 			pStmt = conn.prepareStatement(sql);
-		} catch (SQLException e) {
 
-			e.printStackTrace();
-		}
+			// SQL文を実行し、結果表を取得する
+			ResultSet rs = pStmt.executeQuery();
 
-		// SQL文を実行し、結果表を取得する
-		ResultSet rs = pStmt.executeQuery();
-
-		// 結果表をコレクションにコピーする
-		try {
+			// 結果表をコレクションにコピーする
 			while (rs.next()) {
 				s_question question = new s_question(
 						rs.getInt("question_id"),
@@ -57,16 +42,21 @@ public class s_questionDao {
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
-		}
-
-		// データベースを切断
-		if (conn != null) {
-			try {
-				conn.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
+			questionList = null;
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			questionList = null;
+		} finally {
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
 			}
 		}
+		// 結果を返す
 		return questionList;
 	}
 }
--- Checker_man/src/servlet/User_SindanServlet.java
+++ Checker_man/src/servlet/User_SindanServlet.java
@@ -10,6 +10,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import dao.s_resultDao;
+import model.Result;
+import model.s_result;
+
 @WebServlet("/User_SindanServlet")
 public class User_SindanServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -43,6 +47,17 @@ public class User_SindanServlet extends HttpServlet {
 		request.setCharacterEncoding("UTF-8");
 		String question_id = request.getParameter("QUESTION_ID");
 		String question = request.getParameter("QUESTION");
+
+		// 登録処理を行う
+		s_resultDao bDao = new s_resultDao();
+		if (bDao.insert(
+				new s_result(question_id, question))) { // 登録成功
+			request.setAttribute("result",
+					new Result("登録成功", "レコードを登録しました。", "/Checker_man/User_ResultServlet"));
+		} else { // 登録失敗
+			request.setAttribute("result",
+					new Result("登録失敗", "レコードを登録できませんでした。", "/Checker_man/User_SindanServlet"));
+		}
 
 		// 結果ページにフォワードする
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/User_Result.jsp");
