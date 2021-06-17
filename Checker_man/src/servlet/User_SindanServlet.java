01f17ac7d2f8f116892a1b45fddaa80211eb3fbd
dojo2021-M-Suzuki <suzuki-ma@seplus2016.onmicrosoft.com>
2021-06-17 13:17:59 +0900

    変更

--- Checker_man/src/servlet/User_SindanServlet.java
+++ Checker_man/src/servlet/User_SindanServlet.java
@@ -8,8 +8,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
+import dao.s_answerDao;
 import model.Result;
 import model.s_answer;
 
@@ -20,26 +20,30 @@ public class User_SindanServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		// もしもログインしていなかったらログインサーブレットにリダイレクトする
-		HttpSession session = request.getSession();
-		if (session.getAttribute("user_id") == null) {
-			response.sendRedirect("/Checker_man/User_SindanServlet");
-			return;
-		}
-
+		//		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+		//		HttpSession session = request.getSession();
+		//		if (session.getAttribute("user_id") == null) {
+		//			response.sendRedirect("/Checker_man/User_SindanServlet");
+		//			return;
+		//		}
+		//
 		//		// リクエストパラメータを取得する
 		//		request.setCharacterEncoding("UTF-8");
 		//		String question_id = request.getParameter("QUESTION_ID");
 		//		String question = request.getParameter("QUESTION");
 		//
 		//		// 診断項目の処理を行う
-		//		s_question bDao = new s_question();
-		//		if (bDao.select(new s_question(0, question))) { // 登録成功
-		//			request.setAttribute("result",
-		//					new Result("登録成功", "レコードを登録しました。", "/Checker_man/User_ResultServlet"));
-		//		} else { // 登録失敗
-		//			request.setAttribute("result",
-		//					new Result("登録失敗", "レコードを登録できませんでした。", "/Checker_man/User_SindanServlet"));
+		//		s_questionDao bDao = new s_questionDao();
+		//		try {
+		//			if (bDao.select(new s_question(0, question)) != null) { // 登録成功
+		//				request.setAttribute("result",
+		//						new Result("登録成功", "レコードを登録しました。", "/Checker_man/User_ResultServlet"));
+		//			} else { // 登録失敗
+		//				request.setAttribute("result",
+		//						new Result("登録失敗", "レコードを登録できませんでした。", "/Checker_man/User_SindanServlet"));
+		//			}
+		//		} catch (SQLException e) {
+		//			e.printStackTrace();
 		//		}
 
 		// 診断ページにフォワードする
@@ -50,12 +54,12 @@ public class User_SindanServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		// もしもログインしていなかったらログインサーブレットにリダイレクトする
-		HttpSession session = request.getSession();
-		if (session.getAttribute("user_id") == null) {
-			response.sendRedirect("/Checker_man/User_LoginServlet");
-			return;
-		}
+		//		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+		//		HttpSession session = request.getSession();
+		//		if (session.getAttribute("user_id") == null) {
+		//			response.sendRedirect("/Checker_man/User_LoginServlet");
+		//			return;
+		//		}
 
 		// リクエストパラメータを取得する
 		request.setCharacterEncoding("UTF-8");
@@ -65,7 +69,7 @@ public class User_SindanServlet extends HttpServlet {
 		String question = request.getParameter("QUESTION");
 
 		// 診断結果の登録処理を行う
-		s_answer bDao = new s_answer();
+		s_answerDao bDao = new s_answerDao();
 		if (bDao.insert(new s_answer(0, 0, 0, question))) { // 登録成功
 			request.setAttribute("result",
 					new Result("登録成功", "レコードを登録しました。", "/Checker_man/User_ResultServlet"));
