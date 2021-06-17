339c2287a0893525b6f4a4fc2ebdbaa364069677
dojo2021-n-hoshi <hoshi@seplus2016.onmicrosoft.com>
2021-06-17 10:58:04 +0900

    更新

--- Checker_man/src/servlet/User_ResultServlethoshi.java
+++ Checker_man/src/servlet/User_ResultServlethoshi.java
@@ -1,7 +1,6 @@
 package servlet;
 
 import java.io.IOException;
-import java.sql.SQLException;
 import java.util.List;
 
 import javax.servlet.RequestDispatcher;
@@ -10,13 +9,11 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-import dao.s_answerDao;
-import dao.s_questionDao;
 import dao.s_resultDao;
-import model.s_answer;
-import model.s_question;
-import model.s_result;
+import model.LoginUser;
+import model.user_toppage;
 
 /**
  * Servlet implementation class User_ResultServlet
@@ -30,52 +27,61 @@ public class User_ResultServlethoshi extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// もしもログインしていなかったらログインサーブレットにリダイレクトする
-//		HttpSession session = request.getSession();
+		HttpSession session = request.getSession();
 //		if (session.getAttribute("result_id") == null) {
 //			response.sendRedirect("/Checker_man/User_LoginServlet");
 //			return;
 //		}
 
-//		//検索処理を行う
+
+		// 受講者が回答したデータの取得を行う
+		//LoginUser user_id = (LoginUser)session.getAttribute("user_id"); //セッションスコープからデータを入手、JavaBeansと連携させる必要がある
+
+		s_resultDao calenderDao = new s_resultDao();
+
+		// カレンダーの処理をリクエストスコープに格納する
+		request.setAttribute("calenderlist", calenderlist);
+
+		// ユーザートップページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/user_toppage.jsp");
+		dispatcher.forward(request, response);
+		}
+
+
+
+//______________________________________________________________________________________________________
+//
+//		//検索処理を行う（質問内容）
+//		s_questionDao qDao = new s_questionDao();
+//		List<s_question> questionList = null;
+//		try {
+//			questionList = qDao.select(null);
+//		} catch (SQLException e) {
+//			// TODO 自動生成された catch ブロック
+//			e.printStackTrace();
+//		}
+//
+//		// 検索結果をリクエストスコープに格納する
+//		request.setAttribute("questionList", questionList);
+//
+//		//検索処理を行う（回答）
+//		s_answerDao aDao = new s_answerDao();
+//		List<s_answer> answerList = null;
+//		answerList = aDao.select(new s_answer(0, 0, 0, ""));
+//
+//		// 検索結果をリクエストスコープに格納する
+//		request.setAttribute("answerList", answerList);
+//
+//		//検索処理を行う（管理者コメント）
 //		s_resultDao rDao = new s_resultDao();
 //		List<s_result> resultList = rDao.select(new s_result(0, "", "", "", "", 0));
 //
 //		// 検索結果をリクエストスコープに格納する
 //		request.setAttribute("resultList", resultList);
-
-//______________________________________________________________________________________________________
-
-		//検索処理を行う（質問内容）
-		s_questionDao qDao = new s_questionDao();
-		List<s_question> questionList = null;
-		try {
-			questionList = qDao.select(null);
-		} catch (SQLException e) {
-			// TODO 自動生成された catch ブロック
-			e.printStackTrace();
-		}
-
-		// 検索結果をリクエストスコープに格納する
-		request.setAttribute("questionList", questionList);
-
-		//検索処理を行う（回答）
-		s_answerDao aDao = new s_answerDao();
-		List<s_answer> answerList = null;
-		answerList = aDao.select(new s_answer(0, 0, 0, ""));
-
-		// 検索結果をリクエストスコープに格納する
-		request.setAttribute("answerList", answerList);
-
-		//検索処理を行う（管理者コメント）
-		s_resultDao rDao = new s_resultDao();
-		List<s_result> resultList = rDao.select(new s_result(0, "", "", "", "", 0));
-
-		// 検索結果をリクエストスコープに格納する
-		request.setAttribute("resultList", resultList);
-
-		// 診断結果ページにフォワードする
-		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/user_result.jsp");
-		dispatcher.forward(request, response);
+//
+//		// 診断結果ページにフォワードする
+//		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/user_result.jsp");
+//		dispatcher.forward(request, response);
 
 
 	}
