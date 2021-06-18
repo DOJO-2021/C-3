2b071e21a9c1164491b4130e101eb32658322b5e
dojo2021-k-ando <ando@seplus2016.onmicrosoft.com>
2021-06-18 09:18:37 +0900

    変更‐安藤

--- Checker_man/src/test/user_loginDaoTest.java
+++ Checker_man/src/test/user_loginDaoTest.java
@@ -2,8 +2,8 @@ package test;
 
 import java.util.List;
 
-import dao.user_loginDao;
-import model.user_login;
+import dao.User_loginDao;
+import model.User_login;
 
 
 public class user_loginDaoTest {
@@ -13,14 +13,14 @@ public class user_loginDaoTest {
 		testIsLoginOK2();	// ユーザーが見つからない場合のテスト
 
 		//insert()のテスト
-		user_loginDao dao = new user_loginDao();
+		User_loginDao dao = new User_loginDao();
 
 		System.out.println("---------- insert()のテスト ----------");
-		user_login insRec = new user_login(2, "Tanaka_Kana", "4567");
+		User_login insRec = new User_login("Tanaka_Kana", "4567");
 		if (dao.insert(insRec)) {
 			System.out.println("登録成功！");
-			List<user_login> signList3 = dao.select(insRec);
-			for (user_login sign : signList3) {
+			List<User_login> signList3 = dao.select(insRec);
+			for (User_login sign : signList3) {
 				System.out.println("user_id" + sign.getUser_id());
 				System.out.println("user_name" + sign.getUser_name());
 				System.out.println("user_pw" + sign.getUser_pw());
@@ -34,8 +34,8 @@ public class user_loginDaoTest {
 
 	// ユーザーが見つかる場合のテスト
 	public static void testIsLoginOK1() {
-		user_loginDao dao = new user_loginDao();
-		if (dao.isLoginOK(1, "hoshi","nh1234")) {
+		User_loginDao dao = new User_loginDao();
+		if (dao.isLoginOK("hoshi","nh1234")) {
 			System.out.println("testIsLoginOK1：テストが成功しました");
 		}
 		else {
@@ -45,8 +45,8 @@ public class user_loginDaoTest {
 
 	// ユーザーが見つからない場合のテスト
 	public static void testIsLoginOK2() {
-		user_loginDao dao = new user_loginDao();
-		if (!dao.isLoginOK(1, "hoshi","nh1234")) {
+		User_loginDao dao = new User_loginDao();
+		if (!dao.isLoginOK("hoshi","nh1234")) {
 			System.out.println("testIsLoginOK2：テストが成功しました");
 		}
 		else {
