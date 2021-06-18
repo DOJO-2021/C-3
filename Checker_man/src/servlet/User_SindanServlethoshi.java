26cb0713990318f5d3aada318a4c6fe5e8ea5dbf
dojo2021-n-hoshi <hoshi@seplus2016.onmicrosoft.com>
2021-06-18 11:25:50 +0900

    更新

--- Checker_man/src/servlet/User_SindanServlethoshi.java
+++ Checker_man/src/servlet/User_SindanServlethoshi.java
@@ -9,10 +9,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import dao.s_answerDao;
-import model.Result;
-import model.s_answer;
-
 @WebServlet("/User_SindanServlet")
 public class User_SindanServlethoshi extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -62,21 +58,23 @@ public class User_SindanServlethoshi extends HttpServlet {
 		//		}
 
 		// リクエストパラメータを取得する
-		request.setCharacterEncoding("UTF-8");
-		request.getParameter("QUESTION_ID");
-		request.getParameter("ANSWER");
-		request.getParameter("USER_ID");
-		String question = request.getParameter("QUESTION");
+//		request.setCharacterEncoding("UTF-8");
+//		request.getParameter("QUESTION_ID");
+//		request.getParameter("ANSWER");
+//		request.getParameter("USER_ID");
+//		String question = request.getParameter("QUESTION");
+//
+//		// 診断結果の登録処理を行う
+//		s_answerDao bDao = new s_answerDao();
+//		if (bDao.insert(new s_answer(0, 0, 0, question))) { // 登録成功
+//			request.setAttribute("result",
+//					new Result("登録成功", "レコードを登録しました。", "/Checker_man/User_ResultServlet"));
+//		} else { // 登録失敗
+//			request.setAttribute("result",
+//					new Result("登録失敗", "レコードを登録できませんでした。", "/Checker_man/User_SindanServlet"));
+//		}
 
-		// 診断結果の登録処理を行う
-		s_answerDao bDao = new s_answerDao();
-		if (bDao.insert(new s_answer(0, 0, 0, question))) { // 登録成功
-			request.setAttribute("result",
-					new Result("登録成功", "レコードを登録しました。", "/Checker_man/User_ResultServlet"));
-		} else { // 登録失敗
-			request.setAttribute("result",
-					new Result("登録失敗", "レコードを登録できませんでした。", "/Checker_man/User_SindanServlet"));
-		}
+
 
 		// 診断結果ページにフォワードする
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/User_Result.jsp");
