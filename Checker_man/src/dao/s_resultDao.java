5dc44b44f78e89ad721ad9a39d0dec1cf71be3e9
dojo2021-n-hoshi <hoshi@seplus2016.onmicrosoft.com>
2021-06-22 17:52:53 +0900

    更新

--- Checker_man/src/dao/s_resultDao.java
+++ Checker_man/src/dao/s_resultDao.java
@@ -277,6 +277,68 @@ public class s_resultDao {
 			return resultList;
 		}
 
+		// コメントを更新し、成功したらtrueを返す
+				public boolean update_admincomment(s_result param) {
+
+					Connection conn = null;
+					boolean result = false;
+
+					try {
+						// JDBCドライバを読み込む
+						Class.forName("org.h2.Driver");
+
+						// データベースに接続する
+						conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/C-3/database", "sa", "sa");
+
+						// SQL文を準備する
+						String sql = "update s_result set admin_comment= ? where user_id =? and date = CURDATE()";
+						PreparedStatement pStmt = conn.prepareStatement(sql);
+
+						// SQL文を完成させる
+						if (param.getAdmin_comment() != null) {
+							pStmt.setString(1, param.getAdmin_comment());
+						}
+						else {
+							pStmt.setString(1, null);
+						}
+
+						if (param.getUser_id() != 0) {
+							pStmt.setInt(2, param.getUser_id());
+						}
+						else {
+							pStmt.setInt(2, 0);
+						}
+
+						// SQL文を実行する
+						if (pStmt.executeUpdate() == 1) {
+							result = true;
+						}
+
+					}
+
+					catch (SQLException e) {
+						e.printStackTrace();
+					}
+					catch (ClassNotFoundException e) {
+						e.printStackTrace();
+					}
+
+					finally {
+						// データベースを切断
+						if (conn != null) {
+							try {
+								conn.close();
+							}
+							catch (SQLException e) {
+								e.printStackTrace();
+							}
+						}
+					}
+
+					// 結果を返す
+					return result;
+				}
+
 	// カレンダーのデータベース処理
 		public List<user_toppage> select_calender(user_toppage param) {
 
