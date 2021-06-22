dcdd14e7d1bb7e649b41d65d63b19b764eda37fb
dojo2021-minori <ogawa@seplus2016.onmicrosoft.com>
2021-06-22 09:59:04 +0900

    更新

--- Checker_man/src/model/LoginAdmin.java
+++ Checker_man/src/model/LoginAdmin.java
@@ -4,24 +4,24 @@ import java.io.Serializable;
 
 public class LoginAdmin implements Serializable {
 
-	private String admin_id;	// ログイン時のID
+	private int admin_id;	// ログイン時のID
 
 	//引数がないコントラスタ（デフォルトコントラスタ）
 	public LoginAdmin() {
-		this(null);
+		this.admin_id = 0;
 	}
 
 	//引数があるコントラスタ
-	public LoginAdmin(String admin_id) {
+	public LoginAdmin(int admin_id) {
 		this.admin_id = admin_id;
 	}
 
 	//ゲッターセッターのプログラム
-	public String getadmin_id() {
+	public int getAdmin_id() {
 		return admin_id;
 	}
 
-	public void setadmin_id(String admin_id) {
+	public void setAdmin_id(int admin_id) {
 		this.admin_id = admin_id;
 	}
 
--- Checker_man/src/model/admin_login.java
+++ Checker_man/src/model/admin_login.java
@@ -1,45 +1,43 @@
 package model;
 import java.io.Serializable;
 
-public class admin_login implements Serializable {
+public class Admin_login implements Serializable {
 	private int admin_id;
 	private String admin_name;
 	private String admin_pw;
 
 	// 引数があるコンストラクタ
-	public admin_login(int admin_id,String admin_name, String admin_pw) {
-		super();
-		this.admin_id = admin_id;
+	public Admin_login(String admin_name, String admin_pw) {
+
 		this.admin_name = admin_name;
 		this.admin_pw = admin_pw;
 	}
 
-	// 引数がないコンストラクタ（デフォルトコンストラクタ）
-	public admin_login() {
-		super();
-		this.admin_id = 0;
-		this.admin_name = "";
-		this.admin_pw = "";
+	// 引数がないコンストラクタ
+	public Admin_login() {
+
 	}
 
+	//getter
 	public int getAdmin_id() {
 		return admin_id;
-	}
-
-	public void setAdmin_id(int admin_id) {
-		this.admin_id = admin_id;
 	}
 
 	public String getAdmin_name() {
 		return admin_name;
 	}
 
-	public void setAdmin_name(String admin_name) {
-		this.admin_name = admin_name;
-	}
-
 	public String getAdmin_pw() {
 		return admin_pw;
+	}
+
+	//setter
+	public void setAdmin_id(int admin_id) {
+		this.admin_id = admin_id;
+	}
+
+	public void setAdmin_name(String admin_name) {
+		this.admin_name = admin_name;
 	}
 
 	public void setAdmin_pw(String admin_pw) {
