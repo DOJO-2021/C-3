9adea9e09c0eac68b2eaddc4d1e39c0b745714f8
dojo2021-n-hoshi <hoshi@seplus2016.onmicrosoft.com>
2021-06-22 13:06:41 +0900

    更新

--- Checker_man/src/test/User_loginDaoTest.java
+++ Checker_man/src/test/User_loginDaoTest.java
@@ -3,6 +3,7 @@ package test;
 import java.util.List;
 
 import dao.User_loginDao;
+import dao.s_resultDao;
 import model.User_login;
 
 
@@ -16,7 +17,7 @@ public class User_loginDaoTest {
 		User_loginDao dao = new User_loginDao();
 
 		System.out.println("---------- insert()のテスト ----------");
-		User_login insRec = new User_login("Tanaka_Kana", "4567");
+		User_login insRec = new User_login(0,"Tanaka_Kana", "4567");
 		if (dao.insert(insRec)) {
 			System.out.println("登録成功！");
 			List<User_login> signList3 = dao.select(insRec);
@@ -52,5 +53,12 @@ public class User_loginDaoTest {
 		else {
 			System.out.println("testIsLoginOK2：テストが失敗しました");
 		}
+
+		//ユーザー名を表示させるテスト
+		s_resultDao rDao2 = new s_resultDao();
+		User_login result = rDao2.select_username(new User_login(1, "", "" ));
+		System.out.println("名前は、" + result.getUser_name());
+
+
 	}
 }
