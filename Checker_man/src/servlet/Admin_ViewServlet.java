ee354b7ecef191a9c4008b2962a321c724c42efa
dojo2021-n-hoshi <hoshi@seplus2016.onmicrosoft.com>
2021-06-23 09:57:58 +0900

    更新

--- Checker_man/src/servlet/Admin_ViewServlet.java
+++ Checker_man/src/servlet/Admin_ViewServlet.java
@@ -39,6 +39,11 @@ public class Admin_ViewServlet extends HttpServlet {
 			return;
 		}
 
+		if (request.getParameter("user_id") == null) {
+			response.sendRedirect("/Checker_man/Admin_TopPageServlet");
+			return;
+		}
+
 		//URL（Admin_ViewServlet?user_id = ○）から、○の数値を取得
 		int user_id =  Integer.parseInt(request.getParameter("user_id"));
 
